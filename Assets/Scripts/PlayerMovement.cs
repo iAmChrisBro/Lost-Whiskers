d66bdc3bdfb4a6f24db0199c3ce96f0c342dd800
iAmChrisBro <christianbroxD@gmail.com>
2024-09-22 20:56:24 -0700

    Commit 2

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,36 +1,60 @@
 using System.Collections;
+
+
 using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Security.Cryptography;
+using System.Threading;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
+public class ThirdPersonMovement : MonoBehaviour
 {
-    float speed = 5.0f;
-    float speedRotate = 100.0f;
+    public CharacterController controller;
+    public Transform cam;
+
+    public float speed = 6;
+    public float gravity = -9.81f;
+    public float jumpHeight = 3;
+    Vector3 velocity;
+    bool isGrounded;
+
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+
+    float turnSmoothVelocity;
+    public float turnSmoothTime = 0.1f;
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey("w"))
+        //jump
+
+        if (controller.isGrounded && velocity.y < 0)
         {
-            Vector3 movement = new Vector3(0.0f, 0.0f, 1.0f * Time.deltaTime * speed);
-            movement = transform.TransformDirection(movement);
-            transform.position += movement;
+            velocity.y = -2f;
         }
-        if (Input.GetKey("a"))
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
         {
-            Vector3 rotation = new Vector3(0.0f, -1.0f * Time.deltaTime * speedRotate, 0.0f);
-            transform.Rotate(rotation);
+            velocity.y = Mathf.Sqrt(jumpHeight * -2 * gravity);
         }
-        if (Input.GetKey("s"))
+        //gravity
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+        //walk
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+
+        if (direction.magnitude >= 0.1f)
         {
-            Vector3 movement = new Vector3(0.0f, 0.0f, -1.0f * Time.deltaTime * speed);
-            movement = transform.TransformDirection(movement);
-            transform.position += movement;
-        }
-        if (Input.GetKey("d"))
-        {
-            Vector3 rotation = new Vector3(0.0f, 1.0f * Time.deltaTime * speedRotate, 0.0f);
-            transform.Rotate(rotation);
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            controller.Move(moveDir.normalized * speed * Time.deltaTime);
         }
     }
 }
