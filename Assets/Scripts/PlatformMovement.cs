1867139b43a282051cc888f564d09c4daef4d80b
iAmChrisBro <christianbroxD@gmail.com>
2024-10-14 23:26:11 -0700

    Commit 11

--- Assets/Scripts/PlatformMovement.cs
+++ Assets/Scripts/PlatformMovement.cs
@@ -2,39 +2,33 @@ using UnityEngine;
 
 public class PointMovement : MonoBehaviour
 {
-    public Transform[] points;  // Array of points the object will move between
-    public float speed = 2f;    // Speed at which the object will move
-    private int i = 0;          // Index for tracking current point
-    private bool movingForward = true; // To track direction of movement (forward or backward)
+    public float speed;
+    private bool offPlatform;
+    public Transform target;
 
     void Update()
     {
-        if (Vector2.Distance(transform.position, points[i].position) < 0.02f)
+        if(offPlatform)
         {
-
-            // Update the index based on the current direction
-            if (movingForward)
-            {
-                i++;
-                if (i == points.Length) // Reached the last point
-                {
-                    i--; // Step back to stay within bounds
-                    movingForward = false; // Switch direction to backward
-                   
-                }
-            }
-            else
-            {
-                i--;
-                if (i == -1) // Reached the first point
-                {
-                    i++; // Step forward to stay within bounds
-                    movingForward = true; // Switch direction to forward
-                }
-            }
+            transform.position = Vector3.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
         }
+    }
 
-        // Move the object towards the current target point
-        transform.position = Vector2.MoveTowards(transform.position, points[i].position, speed * Time.deltaTime);
+    void OnCollisionEnter(Collision collision)
+    {
+        transform.Translate(Vector3.up * speed * Time.deltaTime);
+        offPlatform = false;
+    }
+
+    void OnCollisionStay(Collision collision)
+    {
+        transform.Translate(Vector3.up * speed * Time.deltaTime);
+        offPlatform = false;
+    }
+
+    void OnCollisionExit(Collision collision)
+    {
+        offPlatform = true;
     }
 }
+  
